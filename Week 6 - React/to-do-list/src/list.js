37a89fa7e8624ccc59a4b2e3f3faa677d84510c8
WarrenPitterson <warrenpitterson@gmail.com>
2019-02-20 17:51:21 +0000

    added input and submit button

--- Week 6 - React/to-do-list/src/list.js
+++ Week 6 - React/to-do-list/src/list.js
@@ -4,10 +4,9 @@ class List extends Component {
     render() {
         return (
             <div className="Items">
-            <h3>List Header </h3>
-            <ul>
-                <li>List{ this.props.List }</li>
-            </ul>
+            <input name="listItem"></input>
+            <button>Submit</button>
+           
                 
             </div>
         )
