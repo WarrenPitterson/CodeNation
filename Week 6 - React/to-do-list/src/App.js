637c162abd37aace6826d1c39ceef11830da85e5
WarrenPitterson <warrenpitterson@gmail.com>
2019-02-20 14:23:08 +0000

    header added

--- Week 6 - React/to-do-list/src/App.js
+++ Week 6 - React/to-do-list/src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import List from './list';
+import Header from './header';
 
 class App extends Component {
   state = {
     list: []
-    }
-   
-   
-   
-   
-    render() {
-      return (
-        <div className="App">
-        <List
-        />
-        </div>
-      );
-    }
   }
-  
 
-  
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <List/>
+        <Header/>
+      </div>
+    );
+  }
+}
+
+
+
 
 
 export default App;
