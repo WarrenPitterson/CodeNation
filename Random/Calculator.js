8bc5e358df8c84ca72faec9815dde8d715554db8
WarrenPitterson <warrenpitterson@gmail.com>
2019-02-21 16:59:01 +0000

    js practice

---
+++ Random/Calculator.js
@@ -0,0 +1,6 @@
+let firstnumber = prompt("please provide the first number");
+let secondnumber = prompt("please provide the second number");
+
+alert(Number(firstnumber) + Number(secondnumber));
+
+
---
+++ Random/else if.js
@@ -0,0 +1,14 @@
+let age = prompt("please enter your age?");
+
+if (age <18) {
+    alert ("too young");
+}
+else if  (age ==18) {
+    alert ("Just about!");
+}
+else if  (age >18) {
+    alert ("go nuts! drive away!!");
+
+}
+
+//keyless car
