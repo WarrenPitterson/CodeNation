b14bd02b9cdbf61cbdb1b36a3f38eaa03c0dc65d
WarrenPitterson <warrenpitterson@gmail.com>
2019-02-22 11:17:11 +0000

    Countdown ammended, comments added

--- Random/Countdown.js
+++ Random/Countdown.js
@@ -1,8 +1,12 @@
+//sets a countdown to the date we want to finish on 
 let countDownDate = new Date("Dec 25, 2019 00:00:00").getTime();
 
+// runs the function, gets the current time and takes it away from the countdownDate
     let run = setInterval(function() {
     let now = new Date().getTime();
     let distance = countDownDate - now;
+
+//works out the days/hours/minutes/seconds     
 
     let days = Math.floor(distance / (1000*60*60*24));
     let hours = Math.floor((distance % (1000*60*60*24)) /(1000*60*60));
@@ -11,8 +15,11 @@ let countDownDate = new Date("Dec 25, 2019 00:00:00").getTime();
 
 }
 
+
+// calls the function every second
 ,1000);
 
 console.log(countDownDate);
 
 
+// incomplete, needs finishing  
