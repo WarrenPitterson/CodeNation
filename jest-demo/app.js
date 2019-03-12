9a11b3699c10417f00ad8d186f8deee70e6ba28c
WarrenPitterson <warrenpitterson@gmail.com>
2019-03-12 16:19:01 +0000

    unit testing

--- jest-demo/app.js
+++ jest-demo/app.js
@@ -2,10 +2,20 @@ function add(a,b){
     return a+b
 };
 
+function subtract(a,b) {
+    return a-b
+};
+
+function multiply(a,b) {
+    return a*b
+};
+
+function division(a,b) {
+    return a/b
+};
+
+
 const myArray = ["Warren", "Bob", "Phil"]
 
-console.log(add(2,3))
-console.log(myArray)
-
-module.exports = {add, myArray}
+module.exports = {add, subtract, multiply, division, myArray}
 
--- jest-demo/test/app.test.js
+++ jest-demo/test/app.test.js
@@ -7,3 +7,15 @@ test('should equal 5 when passed 2 and 3',() => {
 test('should contain Warren',() => {
     expect(app.myArray).toContain("Warren");
 })
+
+test('should equal 1 when passed 3 and 2',() => {
+    expect(app.subtract(3,2)).toBe(1);
+})
+
+test('should equal 6 when passed 2 and 3',() => {
+    expect(app.multiply(2,3)).toBe(6);
+})
+
+test('should equal 2 when passed 4 and 2',() => {
+    expect(app.division(4,2)).toBe(2);
+})
